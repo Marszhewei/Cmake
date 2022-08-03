5942713dc93c72c3a2f5ebeec4754e8aa6cefcce
fetch150zy <Mars_zhewei@outlook.com>
2022-08-03 16:01:24 +0800

    cmake tutorial step3 build dir

---
+++ step3_build/bin/CMakeFiles/Math.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Math.dir/mysqrt.cxx.o"
+  "CMakeFiles/Math.dir/mysqrt.cxx.o.d"
+  "libMath.a"
+  "libMath.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Math.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
